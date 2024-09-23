df094a392ba46f8aad2581d36b66a9d253413297
susana-equihua <susiequihua@gmail.com>
2024-09-22 20:26:23 -0600

    logic of the function guees_the_number without player turns

--- main.py
+++ main.py
@@ -6,27 +6,43 @@ import random as r
 #random_number = r.randint(1 , 100) #Declarar una variable que guarde el numero aleatorio
 #print (random_number)
 
-
-random_number = r.randint(1 , 100)
-#print (f"Número a adivinar: {random_number}")
-
-girlplayer_number = int(input("Ingresa un número aleatorio entre el 1 y 100: "))
-    # int( ) para convertir el valor de input en numero
-#print (girlplayer_number)
-
-computer_player_number = r.randint(1 , 100)
-#print ("Numpero del jugador computadora: " + str(computer_player_number))
-
 #Función principal para el control del juego
 def guess_the_number():
     #Bienvenida al juego
-    print("Bienvenidos a Guess The Number\nAdivina el número al azar que ha creado el ordenador")
-    
-    turno = "jugador"  #Variable para indicar de quién es el turno
-    NUMBER = random_number
-    while True:  #Mientras la expresión sea verdadera
-       if turno == girlplayer_number:  
+    print("Bienvenidos a Guess The Number\nTu o la computadora adivinarán el número correcto")
+    print("Creando el número secreto...")
+    random_number = r.randint(1 , 100)
+    print (f"Número a adivinar: {random_number}")
+    girlplayer_number = int(input("Ingresa un número aleatorio entre el 1 y 100: "))
+    # int( ) para convertir el valor de input en numero
+    #print (girlplayer_number)
+
+    #while girlplayer_number != random_number: #Mientras la expresión sea verdadera
+    while True: #La sentencia puede funcionar tanto para el caso de la jugadora como de la computadora
+        #if girlplayer_number is not None: #Sentencia no necesaria
+        if 1 <= girlplayer_number <= 100: #Verificar que se encuentre dentro del rango
             if girlplayer_number > random_number:
-                print (int(input("El número es menor: ")))
+                girlplayer_number = (int(input("El número es menor: ")))
+            elif girlplayer_number < random_number:
+                girlplayer_number = (int(input("El número es mayor: ")))
+            else:
+                print("¡Felicidades, adivinaste el número correcto!")
+                break #Termina el juego
+        else:
+            girlplayer_number = int(input("Recuerda debe ser un número entre 1 y 100: "))
+
+        computer_player_number = r.randint(1 , 100)
+        print ("Ingresa un número aleatorio del 1 al 100: " + str(computer_player_number))
+        if computer_player_number < random_number:
+            pista = input("Dame una pista, el número es mayor o menor: ")
+        if pista == "mayor":
+            lower_bound = computer_player_number + 1
+            computer_player_number = r.randint (lower_bound, 100)
+        elif pista == "menor":
+            upper_bound = computer_player_number - 1
+            computer_player_number = r.randint(1 , upper_bound)
+        else:
+            print ("¡Felicidades, adivinaste el número correcto!")
+        
 
 guess_the_number()
